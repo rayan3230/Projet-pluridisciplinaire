6be59aeb3da564907dab2370e0f6c9c5ffd5009e
wassimmho <wassim.mho000@gmail.com>
2025-04-12 13:23:43 +0100

    User filter Done

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+// import App from './App.jsx' // Comment out App for now
 import './index.css'
 import { AuthProvider } from './context/AuthContext'
-import Login from './components/Login/Login.jsx'
+import Filter from './components/Filter/Filter.jsx'; // Import Filter
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
       <AuthProvider>
-        <App/>
+        {/* Render Filter component directly for testing */}
+        <Filter />
+        {/* 
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<App />} />
+             Add other routes 
+          </Routes>
+        </BrowserRouter>
+        */}
       </AuthProvider>
 
   </React.StrictMode>,
